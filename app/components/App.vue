c18a12033f1eb7411ff4a3efa14ac40435019c67
Georgi Ivanov <georgi@sellfer.com>
2020-04-18 21:03:51 +0300

    add progress bar for goal

--- app/components/App.vue
+++ app/components/App.vue
@@ -7,8 +7,34 @@
             androidSelectedTabHighlightColor="#ffffff"
         >
             <TabViewItem class="fal menuButton" title.decode="&#xf043; Вода">
-                <GridLayout columns="*" rows="*">
-                    <Label class="fal" col="0" row="0" />
+                <GridLayout rows="*, 300" columns="*">
+                    <CircularProgressBar
+                        row="0"
+                        rowSpan="0"
+                        col="0"
+                        :size="300"
+                        :progress="goalProgress"
+                    />
+                    <StackLayout
+                        background="grey"
+                        marginTop="0"
+                        borderRadius="999"
+                        width="265"
+                        height="265"
+                        row="0"
+                        col="0"
+                        >.
+                        <Label
+                            class="intakeIndicatorLabel"
+                            :text="currentWater + ' / ' + waterGoal + ' мл'"
+                        />
+                        <Button width="100" text="Пийни" @tap="takeWater()" />
+                        <Button
+                            width="100"
+                            text="Плювни"
+                            @tap="removeWater()"
+                        />
+                    </StackLayout>
                 </GridLayout>
             </TabViewItem>
             <TabViewItem class="fal menuButton" title.decode="&#xf1da; История">
@@ -21,7 +47,10 @@
                     />
                 </GridLayout>
             </TabViewItem>
-            <TabViewItem class="fal menuButton" title.decode="&#xf013; Настройки">
+            <TabViewItem
+                class="fal menuButton"
+                title.decode="&#xf013; Настройки"
+            >
                 <GridLayout columns="*" rows="*">
                     <Label
                         class="message"
@@ -36,11 +65,48 @@
 </template>
 
 <script>
+import CircularProgressBar from "./_shared/CircularProgressBar";
 export default {
+    components: {
+        CircularProgressBar,
+    },
     data() {
-        return {};
+        return {
+            waterGoal: 1200,
+            currentWater: 0,
+        };
+    },
+    computed: {
+        goalProgress: function() {
+            return (this.currentWater * 100) / this.waterGoal;
+        },
+    },
+    methods: {
+        takeWater() {
+            if (this.currentWater + 200 >= this.waterGoal) {
+                this.currentWater = this.waterGoal;
+            } else {
+                this.currentWater = this.currentWater + 200;
+            }
+        },
+        removeWater() {
+            if (this.currentWater - 200 <= 0) {
+                this.currentWater = 0;
+            } else {
+                this.currentWater = this.currentWater - 200;
+            }
+        },
     },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.intakeIndicatorLabel {
+    width: 100%;
+    text-align: center;
+    color: #fff;
+    margin-top: 100;
+    margin-bottom: 15;
+    font-size: 25;
+}
+</style>
---
+++ app/components/_shared/CircularProgressBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <GridLayout :height="height" :width="height">
+        <RadRadialGauge>
+            <RadialScale
+                v-tkRadialGaugeScales
+                startAngle="180"
+                sweepAngle="180"
+            >
+                <ScaleStyle
+                    v-tkRadialScaleStyle
+                    ticksVisible="false"
+                    labelsVisible="false"
+                    lineThickness="0"
+                >
+                </ScaleStyle>
+
+                <RadialBarIndicator
+                    v-tkRadialScaleIndicators
+                    minimum="0"
+                    maximum="100"
+                >
+                    <BarIndicatorStyle
+                        v-tkRadialBarIndicatorStyle
+                        :fillColor="fillBackgroundColor"
+                        cap="Round"
+                        barWidth="0.05"
+                    >
+                    </BarIndicatorStyle>
+                </RadialBarIndicator>
+
+                <RadialBarIndicator
+                    v-tkRadialScaleIndicators
+                    minimum="0"
+                    :maximum="value"
+                    isAnimated="true"
+                >
+                    <BarIndicatorStyle
+                        v-tkRadialBarIndicatorStyle
+                        :fillColor="fillColor"
+                        cap="Round"
+                        barWidth="0.05"
+                    >
+                    </BarIndicatorStyle>
+                </RadialBarIndicator>
+            </RadialScale>
+        </RadRadialGauge>
+    </GridLayout>
+</template>
+
+<script>
+export default {
+    props: {
+        size: { default: 100 },
+        progress: { default: 0 },
+        offset: { default: 0 },
+        textColor: { default: "#bfbfc4" },
+        fillColor: { default: "#4aa9ee" },
+        fillBackgroundColor: { default: "#efeff4" },
+    },
+    computed: {
+        height() {
+            return Math.min(this.size, 450);
+        },
+        value() {
+            return Math.min(this.progress, 100);
+        },
+    },
+};
+</script>
+
+<style></style>
